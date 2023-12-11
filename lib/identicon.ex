9693212ee2cee0add7322cc64d0fff4b02594d28
David Nuertey Manor <manornuertey@gmail.com>
2023-12-11 19:04:27 +0000

    implement creating svg

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,17 +1,33 @@
 defmodule IDENTICON do
+  defp create_svg(grid_data, id) do
+    svg_content = """
+    <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
+      #{generate_grid(grid_data)}
+    </svg>
+    """
+
+    File.write!("tmp/#{id}_#{System.os_time(:second)}.svg", svg_content)
+  end
+
+  @spec generate_grid(list()) :: String.t()
+  defp generate_grid(grid_data) do
+    Enum.reduce(grid_data, {0, ""}, fn value, {acc, svg} ->
+      {acc + 1,
+       svg <>
+         case rem(value, 2) do
+           0 -> "<rect width=\"100\" height=\"100\" x=\"#{rem(acc, 3) * 100}\" y=\"#{div(acc, 3) * 100}\" fill=\"black\" />"
+           1 -> "<rect width=\"100\" height=\"100\" x=\"#{rem(acc, 3) * 100}\" y=\"#{div(acc, 3) * 100}\" fill=\"white\" />"
+         end
+      }
+    end)
+    |> elem(1)
+  end
+
   @spec gen_i_o(binary()) :: list()
   def gen_i_o(string) do
     split_string = string |> String.graphemes()
 
-    Enum.map(split_string, fn x ->
-      case rem(String.to_integer(x), 2) === 0 do
-        true ->
-          0
-
-        false ->
-          1
-      end
-    end)
+    Enum.map(split_string, &String.to_integer/1)
   end
 
   def create do
@@ -19,7 +35,8 @@ defmodule IDENTICON do
 
     case String.length(id) === 9 do
       true ->
-        gen_i_o(id)
+        grid_data = gen_i_o(id)
+        create_svg(grid_data, id)
 
       false ->
         {:error, "invalid ID"}
