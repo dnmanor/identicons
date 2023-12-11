816ed351317fc113f9178891b6ba9e1f43c29833
David Nuertey Manor <manornuertey@gmail.com>
2023-12-11 19:14:12 +0000

    implement colours for more uniqueness

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,21 +1,21 @@
 defmodule IDENTICON do
   defp create_svg(grid_data, id) do
+    colour = "##{String.slice(id, 0, 6)}"
     svg_content = """
     <svg width="300" height="300" xmlns="http://www.w3.org/2000/svg">
-      #{generate_grid(grid_data)}
+      #{generate_grid(grid_data, colour)}
     </svg>
     """
-
     File.write!("tmp/#{id}_#{System.os_time(:second)}.svg", svg_content)
   end
 
-  @spec generate_grid(list()) :: String.t()
-  defp generate_grid(grid_data) do
+  @spec generate_grid(list(), binary()) :: String.t()
+  defp generate_grid(grid_data, colour) do
     Enum.reduce(grid_data, {0, ""}, fn value, {acc, svg} ->
       {acc + 1,
        svg <>
          case rem(value, 2) do
-           0 -> "<rect width=\"100\" height=\"100\" x=\"#{rem(acc, 3) * 100}\" y=\"#{div(acc, 3) * 100}\" fill=\"black\" />"
+           0 -> "<rect width=\"100\" height=\"100\" x=\"#{rem(acc, 3) * 100}\" y=\"#{div(acc, 3) * 100}\" fill=\"#{colour}\" />"
            1 -> "<rect width=\"100\" height=\"100\" x=\"#{rem(acc, 3) * 100}\" y=\"#{div(acc, 3) * 100}\" fill=\"white\" />"
          end
       }
